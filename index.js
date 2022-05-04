78f1339ba58f00c308d1e88776b1f94a0ba51790
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2022-05-04 20:27:35 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 (async() => {
-
-const puppeteer = require('puppeteer')
-const http = require('http')
-const forward = require('http-forward')
-const app = require('express')()
-const cors = require('cors')
-const server = http.createServer(app)
-app.use(cors())
+const httpProxy = require("http-proxy")
+const host = "0.0.0.0";
 const port = process.env.PORT || 8080 || 5000 || 3000
+async function createServer(WSEndPoint, host, port) {
+  await httpProxy
+    .createServer({
+      target: WSEndPoint, // where we are connecting
+      ws: true,
+      localAddress: host // where to bind the proxy
+    })
+    .listen(port); // which port the proxy should listen to
+  return `ws://${host}:${port}`; // ie: ws://123.123.123.123:8080
+}
+const puppeteer = require('puppeteer')
 const browser = await puppeteer.launch({
 	//executablePath: process.cwd()+'',
 	headless: true,
@@ -46,13 +51,9 @@ const browser = await puppeteer.launch({
 	]
 })
 const browserWSEndpoint = await browser.wsEndpoint()
-app.use((req, res) => {
-  req.forward = { target: browserWSEndpoint.split('/')[2] }
-  forward(req, res)
-})
-server.listen(port, async() => {
-	const pagesCount = (await browser.pages()).length // just to make sure we have the same stuff on both place
-	console.log({ browserWSEndpoint, pagesCount })
-})
+const pagesCount = (await browser.pages()).length; // just to make sure we have the same stuff on both place
+const browserWSEndpoint = await browser.wsEndpoint();
+const customWSEndpoint = await createServer(browserWSEndpoint, host, port); // create the server here
+console.log({ browserWSEndpoint, customWSEndpoint, pagesCount });
 
 })()
